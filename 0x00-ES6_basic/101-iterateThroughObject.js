2eaaecafc7941b07f43065823348e2232ea8a292
YAOSTEPHANE <106774656+YAOSTEPHANE@users.noreply.github.com>
2023-04-17 17:47:31 +0000

    Update 101-iterateThroughObject.js

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,10 +1,13 @@
-export default function createReportObject(employeesList) {
-  return {
-    allEmployees: {
-      ...employeesList,
-    },
-    getNumberOfDepartments(employeesList) {
-      return Object.keys(employeesList).length;
-    },
-  };
+export default function iterateThroughObject(reportWithIterator) {
+	let employees = '';
+
+	for (const [i, value] of reportWithIterator.entries()) {
+		if (i !== reportWithIterator.length - 1) {
+			employees += `${value} | `;
+		} else {
+			employees += value;
+		}
+	}
+
+	return employees;
 }
