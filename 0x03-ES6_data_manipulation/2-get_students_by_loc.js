aca9be050c18098ecb854520f2092f57473c100c
YAOSTEPHANE <106774656+YAOSTEPHANE@users.noreply.github.com>
2023-04-25 02:42:45 +0000

    Create 2-get_students_by_loc.js

---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,17 @@
+/**
+ * Récupère les étudiants dans un endroit donné.
+ * @param {{
+ * id: Numéro,
+ * prénom : chaîne,
+ * emplacement: String
+ * }[]} students - La liste des étudiants.
+ * @param {String} city - L’emplacement.
+ * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * @returns
+ */
+exporter la fonction par défaut getStudentsByLocation(students, city) {
+  if (student instanceof Array) {
+    les étudiants de retour. filter((étudiant) => élève. Localisation === Ville);
+  }
+  rendre [];
+}
