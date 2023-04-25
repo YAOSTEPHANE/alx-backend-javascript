776946563599d0ffaa06fae80e500b33005f8f78
YAOSTEPHANE <106774656+YAOSTEPHANE@users.noreply.github.com>
2023-04-25 02:57:36 +0000

    Update 100-weak.js

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -14,7 +14,7 @@ const MAX_ENDPOINT_CALLS = 5;
  *   protocol: String,
  *   name: String,
  * }} endpoint - The endpoint to make a request to.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * @author YAO KOUASSI STÉPHANE <https://github.com/YAOSTEPHANE>
  */
 export function queryAPI(endpoint) {
   if (!weakMap.has(endpoint)) {
