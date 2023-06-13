78efa3fb6aa1fdc7483e90a505a9269af45e2197
YAOSTEPHANE <kouassistephane489@gmail.com>
2023-06-13 08:51:11 +0000

    Node_JS_basic

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Yao kouassi stephane <https://github.com/YAOSTEPHANE>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
